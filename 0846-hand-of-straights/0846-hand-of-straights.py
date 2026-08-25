f1f32a9e46b43320412bd0b22f3ffaa739563ace
ADITYA BANDEWAR <adityabandewar21@gmail.com>
2026-08-25 20:17:04 +0530

    Time: 51 ms (65.98%), Space: 14.4 MB (42.97%) - LeetHub

---
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def isNStraightHand(self, hand, groupSize):
+        count_map = {}
+        for card in hand:
+            count_map[card] = count_map.get(card, 0) + 1
+        
+        hand.sort()
+        
+        for i in range(len(hand)):
+            if count_map[hand[i]] == 0:
+                continue
+            
+            for j in range(groupSize):
+                curr_card = hand[i] + j
+                
+                if count_map.get(curr_card, 0) == 0:
+                    return False
+                
+                count_map[curr_card] -= 1
+        
+        return True
