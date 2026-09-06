ec311c7a02550e791cf87ea8b787126970c4e328
ADITYA BANDEWAR <adityabandewar21@gmail.com>
2026-09-06 06:50:04 +0530

    Time: 41 ms (13.82%), Space: 65.5 MB (9.03%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int n=s.size();
+        int m=t.size();
+        vector<vector<__int128>>dp(n+1,vector<__int128>(m+1,0));
+        dp[0][0]=1;
+
+
+        for(int i=1;i<=n;i++){
+            dp[i][0]=1;
+
+            for(int j=1;j<=m;j++){
+                dp[i][j]=dp[i-1][j];
+
+                if(s[i-1] == t[j-1]){
+    if(dp[i-1][j-1] > INT_MAX - dp[i-1][j])
+        dp[i][j] = INT_MAX;
+    else
+        dp[i][j] += dp[i-1][j-1];
+}
+            }
+        }
+        return dp[n][m];
+        
+    }
+};
