fa1759c87707b029a7a0fe4307ac317cc0d7bf5f
ADITYA BANDEWAR <adityabandewar21@gmail.com>
2026-09-25 08:08:23 +0530

    Time: 9 ms (16.24%), Space: 71.6 MB (66.73%) - LeetHub

--- 0848-shifting-letters/0848-shifting-letters.cpp
+++ 0848-shifting-letters/0848-shifting-letters.cpp
@@ -2,13 +2,15 @@ class Solution {
 public:
     string shiftingLetters(string s, vector<int>& shifts) {
         int n=shifts.size();
-        for(int i=n-2;i>-1;i--){
-            shifts[i]=(shifts[i]+shifts[i+1])%26;
-            cout<<shifts[i]<<endl;
-        }
-        for(int i=0;i<n;i++){
+        int sum=0;
+        for(int i=n-1;i>-1;i--){
+            shifts[i]=(shifts[i]+sum)%26;
+            sum=shifts[i];
             s[i] = (char)('a' + (s[i] - 'a' + shifts[i]) % 26);
-        }
+         }
+        // for(int i=0;i<n;i++){
+           
+        // }
 
         return s;
     }
